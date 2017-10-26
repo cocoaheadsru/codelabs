e48f1464607e73911276eec7d68b45f97c349d22
Sam Mejlumyan <smejlum@gmail.com>
2017-10-27 02:19:29 +0300

    Add two sum playground

---
+++ Algorithms/TwoSum.playground/Contents.swift
@@ -0,0 +1,20 @@
+/*
+ MBLTdev CodeLabs by CocoaHeads
+ Algoritms
+ Two Sum
+ */
+
+import Foundation
+import PlaygroundSupport
+
+let integers: [Int] = [2, 5, 4, 12, 56, 94, 23, 3, 6, 7, 9, 10]
+
+/*
+ Фукция должна возвращать индексы двух чисел из массива array,
+ сумма которых равна переданному значению target
+ */
+func findTarget(_ target: Int, in array: [Int]) -> [Array<Int>.Index] {
+    // Your implementation here
+
+    return [0, 1]
+}
