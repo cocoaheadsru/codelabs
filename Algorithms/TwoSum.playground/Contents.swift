94e3c5f371bf6d25ad008e388f87d4518c8eceb7
Sam Mejlumyan <smejlum@gmail.com>
2017-10-27 02:22:35 +0300

    Change comments

--- Algorithms/TwoSum.playground/Contents.swift
+++ Algorithms/TwoSum.playground/Contents.swift
@@ -7,14 +7,17 @@
 import Foundation
 import PlaygroundSupport
 
-let integers: [Int] = [2, 5, 4, 12, 56, 94, 23, 3, 6, 7, 9, 10]
+let integers: [Int] = [2, 5, 4, 12, 7]
 
 /*
  Фукция должна возвращать индексы двух чисел из массива array,
  сумма которых равна переданному значению target
  */
 func findTarget(_ target: Int, in array: [Int]) -> [Array<Int>.Index] {
-    // Your implementation here
+    // Ваша реализация
 
     return [0, 1]
 }
+
+// Ожидаемый ответ [2, 3]
+findTarget(16, in: integers)
