8c9b015cc47e11462f1cb1d40c89074d732c9227
Sam Mejlumyan <smejlum@gmail.com>
2017-10-27 02:56:40 +0300

    Add second files

---
+++ Algorithms/ValidPalindrome.playground/Contents.swift
@@ -0,0 +1,29 @@
+/*
+ MBLTdev CodeLabs by CocoaHeads
+ Algoritms
+ Valid Palindrome
+ */
+
+import Foundation
+
+/*
+ Фукция должна возвращать true, если переданная строка - палиндром
+ Палиндром – число, буквосочетание, слово или текст, одинаково читающееся в обоих направлениях.
+ Например, mam, 105501
+ При поиске учитываются только буквы.
+ */
+
+func isPalindrome(_ string: String) -> Bool {
+    // Ваша реализация
+
+    return false
+}
+
+let anyString = "CocoaHeads Russia"
+let palindrome = "Bombard a drab mob."
+
+// Ожидаем false
+isPalindrome(anyString)
+
+// Ожидаем true
+isPalindrome(palindrome)
