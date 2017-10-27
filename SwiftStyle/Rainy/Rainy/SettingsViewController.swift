534e890a88e98bfcc7858c80a487e9682f6912fd
Andrey Khyshov <andreykhyshov@MacBook-Pro-Mikhail-Kogan.local>
2017-10-27 12:14:53 +0300

    Swifty style

--- SwiftStyle/Rainy/Rainy/SettingsViewController.swift
+++ SwiftStyle/Rainy/Rainy/SettingsViewController.swift
@@ -9,28 +9,27 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var swifthLocation: UISwitch!
-    
-    @IBAction func saveButtonPressed(_ sender: Any) {
-        self.view.endEditing(true)
-        setts.set(textField.text!, forKey: "cityName")
+  @IBOutlet weak var textField: UITextField!
+  
+  @IBAction func saveButtonPressed(_ sender: Any) {
+    self.view.endEditing(true)
+    setts.set(textField.text ?? "", forKey: "cityName")
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    textField.resignFirstResponder()
+  }
+  
+  private var setts = UserDefaults.standard
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let name = setts.value(forKey: "cityName"){
+      textField.text = String(describing: name)
     }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        textField.resignFirstResponder()
+    else{
+      textField.text = "Saint Petersburg"
     }
-    
-    private var setts = UserDefaults.standard
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if let name = setts.value(forKey: "cityName"){
-            textField.text = String(describing: name)
-        }
-        else{
-            textField.text = "Saint Petersburg"
-        }
-    }
+  }
 
-    
+  
 }
--- SwiftStyle/Rainy/Rainy/WeatherForecast.swift
+++ SwiftStyle/Rainy/Rainy/WeatherForecast.swift
@@ -8,33 +8,32 @@
 
 import Foundation
 
-
 class WeatherForecast{
-    var currentWeatherTempurature: Double?
-    var timeStamp: String
-    var imageName: String
-    var locationCoordinates: (Double, Double)?
-    var humidity: Int?
-    var pressure: Int?
-    var wind: Double?
-    var cityName: String?
-    var stateWeather: String?
+  var currentWeatherTempurature: Double?
+  var timeStamp: String
+  var imageName: String
+  var locationCoordinates: (Double, Double)?
+  var humidity: Int?
+  var pressure: Int?
+  var wind: Double?
+  var cityName: String?
+  var stateWeather: String?
+  
+  init(currentWeatherTempurature: Double?,
+     timeStamp: String, imageName: String,
+     locationCoordinates: (Double, Double)?, humidity: Int?, pressure: Int?,
+     wind: Double?, cityName: String?, stateWeather: String?) {
     
-    init(currentWeatherTempurature: Double?,
-         timeStamp: String, imageName: String,
-         locationCoordinates: (Double, Double)?, humidity: Int?, pressure: Int?,
-         wind: Double?, cityName: String?, stateWeather: String?) {
-        
-        self.currentWeatherTempurature = currentWeatherTempurature
-        self.timeStamp = timeStamp
-        self.imageName = imageName
-        self.locationCoordinates = locationCoordinates
-        self.humidity = humidity
-        self.pressure = pressure
-        self.wind = wind
-        self.cityName = cityName
-        self.stateWeather = stateWeather
-    }
+    self.currentWeatherTempurature = currentWeatherTempurature
+    self.timeStamp = timeStamp
+    self.imageName = imageName
+    self.locationCoordinates = locationCoordinates
+    self.humidity = humidity
+    self.pressure = pressure
+    self.wind = wind
+    self.cityName = cityName
+    self.stateWeather = stateWeather
+  }
 }
 
 
